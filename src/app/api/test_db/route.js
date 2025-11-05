c1e1617eef49c1a10bf18cedd92fce28d80129ee
mse-lunani <michael.se-lunani@riarauniversity.ac.ke>
2025-11-05 11:39:38 +0300

    bulk upload

--- src/app/api/test_db/route.js
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-
-export async function GET() {
-  const sql = neon(`${process.env.DATABASE_URL}`);
-  const result = await sql`SELECT * FROM admins`;
-  return Response.json(result);
-}
